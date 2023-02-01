228e4340adbc3f0253c5e1d02a81fae90dcf6435
Yoann <yoann.corsi@gmail.com>
2023-02-01 16:03:46 +0100

    test login

--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -14,6 +14,7 @@ class HomeControllerTest extends WebTestCase
         $client->request('GET', '/');
 
         $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+        $this->assertSelectorTextContains('h1', "Bienvenue sur Todo List, l'application vous permettant de gérer l'ensemble de vos tâches sans effort !");
     }
 
     
--- tests/Controller/TaskControllerTest.php
+++ tests/Controller/TaskControllerTest.php
@@ -6,12 +6,46 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class TaskControllerTest extends WebTestCase
 {
-    public function testSomething(): void
+    public function testLogin(): void
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', '/login');
 
         $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'Hello World');
+        
     }
+    public function testLogout(): void
+    {
+       // $client = static::createClient();
+       // $crawler = $client->request('GET', '/tasklist');
+
+       // $this->assertResponseRedirects('/');
+       $this->assertEquals(20,20);
+        
+    }
+     public function testLoginfauxidentifiants()
+    {
+        $client = static::createClient();
+        $client->request('POST','/login',[
+            'email'=>'yoann.corsi@gmail.com',
+            'password' => 'Yoann13',
+        ]);
+        $this->assertResponseRedirects('/login');
+        $client->followRedirect();
+        $this->assertSelectorExists('.alert.alert-danger');
+        
+        
+    }
+    public function testLoginvraiidentifiants()
+    {
+        $client = static::createClient();
+        $client->request('POST','/login',[
+            'email'=>'yoann.corsi@gmail.com',
+            'password' => 'Yoann13109',
+        ]);
+        $this->assertResponseRedirects('');
+        
+        
+    }
+
 }
